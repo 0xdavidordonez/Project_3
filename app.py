f0ef65e1e95a94a69d7473e68b6ead5806e0b756
0xNullLight <37184669+0xNullLight@users.noreply.github.com>
2024-05-02 03:04:27 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import openai
+from PIL import Image
+import streamlit as st
+import os                                          # Needed for dotenv
+from dotenv import load_dotenv                     # To load key.env correctly
+
+# Load environment variables from key.env file
+load_dotenv("key.env")
+
+# Retrieve API key from environment variable
+openai_api_key = os.getenv("openAI_apikey")
+
+# Check if the API key is properly loaded
+if openai_api_key is None:
+    raise ValueError("OpenAI API key is not provided. Please check your key.env file.")
+
+# Initialize OpenAI client with API key
+client = openai.Client(api_key=openai_api_key)
+
+def generate_image(image_description):
+    try:
+        img_response = client.images.generate(
+            model="dall-e-3",
+            prompt=image_description,
+            size="1024x1024",
+            quality="standard",
+            n=1  # Always generate only one image
+        )
+        image_url = img_response.data[0].url
+        return image_url
+    except Exception as e:
+        st.error(f"Error generating image: {e}")
+
+st.set_page_config(page_title="DALL-E-3 Image Generation", page_icon=":camera:", layout="wide")
+
+# Create a title
+st.title("DALL-E-3 Image Generation Tool")
+
+st.subheader("POWERED BY OPENAI!!!!")
+img_description = st.text_input("Enter a description for the image you want to generate:")
+
+# Create a button to generate images
+if st.button("Generate Image") and img_description:
+    generated_image_url = generate_image(img_description)
+    st.image(generated_image_url)
